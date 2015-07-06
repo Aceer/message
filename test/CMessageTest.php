87c730ae7cadf26b6f41137b020f61eb8e50c1a5
Alex <alexander_svensson85@hotmail.com>
2015-07-06 10:13:59 +0200

    Added test dir

---
+++ test/CMessageTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Aceer\Message;
+
+class CMessageTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	* Test 
+	*
+	* @return void
+	*
+	*/
+	public function testSucccessClass()
+	{
+		$e = new \Aceer\CMessage();
+		$e->success('Hej');
+		$t1 = $_SESSION['displayMessage'];
+		$t2 = "<div id='displayMessage' class='alert alert-success'>Hej</div>";
+
+
+		$this->assertEquals($t1, $t2, "Error --------> success message");
+	}
+};
