d8af78ce23b96c7fa326412aba1fbb65cf53e4dd
Alex <alexander_svensson85@hotmail.com>
2015-07-06 12:55:05 +0200

    Update

--- test/CMessageTest.php
+++ test/CMessageTest.php
@@ -13,11 +13,87 @@ class CMessageTest extends \PHPUnit_Framework_TestCase
 	public function testSucccessClass()
 	{
 		$e = new \Aceer\Message\CMessage();
-		$e->success('Hej');
+		$e->success('Success');
 		$t1 = $_SESSION['displayMessage'];
-		$t2 = "<div id='displayMessage' class='alert alert-success'>Hej</div>";
+		$t2 = "<div id='displayMessage' class='alert alert-success'>Success</div>";
 
 
 		$this->assertEquals($t1, $t2, "Error --------> success message");
+	}
+
+		/**
+	* Test 
+	*
+	* @return void
+	*
+	*/
+	public function testDangerClass()
+	{
+		$e = new \Aceer\Message\CMessage();
+		$e->danger('Danger');
+		$t1 = $_SESSION['displayMessage'];
+		$t2 = "<div id='displayMessage' class='alert alert-danger'>Danger</div>";
+
+
+		$this->assertEquals($t1, $t2, "Error --------> danger message");
+	}
+
+		/**
+	* Test 
+	*
+	* @return void
+	*
+	*/
+	public function testWarningClass()
+	{
+		$e = new \Aceer\Message\CMessage();
+		$e->warning('Warning');
+		$t1 = $_SESSION['displayMessage'];
+		$t2 = "<div id='displayMessage' class='alert alert-warning'>Warning</div>";
+
+
+		$this->assertEquals($t1, $t2, "Error --------> Warning message");
+	}
+
+	/**
+	* Test 
+	*
+	* @return void
+	*
+	*/
+	public function testinfoClass()
+	{
+		$e = new \Aceer\Message\CMessage();
+		$e->info('Info');
+		$t1 = $_SESSION['displayMessage'];
+		$t2 = "<div id='displayMessage' class='alert alert-info'>Info</div>";
+
+
+		$this->assertEquals($t1, $t2, "Error --------> info message");
+	}
+
+		/**
+	* Test 
+	*
+	* @return void
+	*
+	*/
+	public function testFalseClass1()
+	{
+		$ar = [
+				'danger' => 'myDanger',
+				'success' => 'mySucces',
+				'info' => 'myInfo',
+				'warning' => 'myWarning'
+			];
+
+		$e = new \Aceer\Message\CMessage($ar);
+
+		$e->success('Success');
+		$t1 = $_SESSION['displayMessage'];
+		$t2 = "<div id='displayMessage' class='mySuccess'>Success</div>";
+
+
+		$this->assertFalse($t1, $t2, "Error --------> false custom class success");
 	}
 };
